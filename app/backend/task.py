22fa083ee0fee47baa2136568dc0b0c8f447c4bd
ezhowww <ezhoff.dmitriy@gmail.com>
2025-03-15 00:42:26 +0300

    work on model

--- app/backend/task.py
+++ app/backend/task.py
@@ -1,3 +1,23 @@
+from dataclasses import dataclass, field
+from datetime import datetime
 
+NOW = datetime(1, 1, 1, 0, 0, 0)
+
+
+@dataclass
+class DateTimeTask:
+    date_set_task: datetime = NOW
+    date_start_task: datetime = NOW
+    time_to_do_task: datetime = NOW
+    deadline: datetime = NOW
+
+
+@dataclass
 class Task:
-    pass
+    text: str = "Empty task"
+    lvl: int = 0
+    tags: list = field(default_factory=list)
+    complexity: float = 0.0
+    priority: float = 0.0
+    date_time_task: DateTimeTask = field(default_factory=DateTimeTask)
+    subtasks: list = field(default_factory=list)
