a577707f998fb20105c8c71a3103390f443ddb7e
ezhowww <ezhoff.dmitriy@gmail.com>
2025-06-29 13:42:05 +0300

    Add user to core_lib

--- core_lib/core_lib/models/user.py
+++ core_lib/core_lib/models/user.py
@@ -1,23 +1,34 @@
+from datetime import datetime
 from typing import Optional
 
-from pydantic import BaseModel, EmailStr, Field
+from pydantic import BaseModel, EmailStr
 
 
-class User(BaseModel):
-    """
-    Main user class model.
-    """
+class UserBase(BaseModel):
+    """This class is a base for user-related models and includes fields that are common to all users."""  # noqa: E501
 
-    id: Optional[int] = None
+    username: str
+    email: EmailStr
+    created_at: datetime = datetime.now()
+    telegram_account: Optional[str] = None
 
-    name: str = Field(..., min_length=3, max_length=128)
 
-    # a more detailed description of the task, optional
-    email: Optional[str] = None
+class UserCreate(UserBase):
+    """This class includes an additional field for a password, used when creating new users. It also has a method to create a hashed version of the password."""  # noqa: E501
 
-    pswd_hash: Optional[EmailStr] = None
+    password: str
 
-    # TODO
 
-    class Config:
-        from_attributes = True
+class UserUpdate(UserBase):
+    """This class includes optional fields for updating user information. It also has a method to update the hashed version of the password if necessary."""  # noqa: E501
+
+    username: Optional[str] = None
+    email: Optional[EmailStr] = None
+    telegram_account: Optional[str] = None
+    password: Optional[str] = None
+
+
+class User(UserBase):
+    """This class represents a finalized user model as stored in the database. It includes all fields from `UserBase` plus a password hash for secure storage of passwords."""  # noqa: E501
+
+    password_hash: str
